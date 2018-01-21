15096d4b61b0de0d413e1e98457d47f4e7f167d4
Gerry Mc Donnell <irishbloke@gmail.com>
2018-01-21 06:48:53 +0000

    total owed working

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -11,17 +11,32 @@ import{Client} from '../../models/client'
 })
 export class ClientsComponent implements OnInit {
 
-  clients:Client[];
+  clients:any[];
+  totalOwed:number;
   
   constructor(
     public clientService:ClientService
-  ) {  }
+  ) {
+    
+    }
 
   ngOnInit() {
     this.clientService.getClients().subscribe(clients =>{
       this.clients=clients;
       console.log(this.clients);
+
+      this.getTotalOwed();
     });
+  }
+
+  getTotalOwed(){
+    let total=0;
+    for(let i=0;i<this.clients.length;i++){
+      total += parseFloat(this.clients[i].balance);
+    }
+    this.totalOwed=total;
+
+    console.log(this.totalOwed);
   }
 
 }
