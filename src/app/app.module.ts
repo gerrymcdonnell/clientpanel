64f2963c16fbbdc7a7418e516d5157b95980343b
Gerry Mc Donnell <irishbloke@gmail.com>
2018-01-21 15:51:52 +0000

    view client details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,7 +43,8 @@ const appRoutes:Routes=[
   {path:'',component:DashboardComponent},
   {path:'register',component:RegisterComponent},
   {path:'login',component:LoginComponent},
-  {path:'add-client',component:AddClientComponent}
+  {path:'add-client',component:AddClientComponent},
+  {path:'client/:id',component:ClientDetailsComponent}
 ];
 
 //firebase credentials from firebase.google
